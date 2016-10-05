164bc2d0dfd6593848475d663191a56ea0653034
cgit <sigit.codeigniter@gmail.com>
2016-10-05 22:10:36 +0700

    Initial commit

---
+++ console/migrations/m161005_145609_create_tabel_kategori.php
@@ -0,0 +1,29 @@
+<?php
+
+use yii\db\Migration;
+
+class m161005_145609_create_tabel_kategori extends Migration
+{
+    public function up()
+    {
+
+    }
+
+    public function down()
+    {
+        echo "m161005_145609_create_tabel_kategori cannot be reverted.\n";
+
+        return false;
+    }
+
+    /*
+    // Use safeUp/safeDown to run migration code within a transaction
+    public function safeUp()
+    {
+    }
+
+    public function safeDown()
+    {
+    }
+    */
+}
